48ece00ea5e145e4926e336150cffe1cd0d9fbb2
dp <codewithdp2073@gmail.com>
2024-12-30 19:44:58 +0530

    service page ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home';
+import About from './Pages/About';
+import Services from './Pages/Services';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
     <>
       <Home />
+      {/* <About /> */}
+      {/* <Services /> */}
+      {/* <Contact/> */}
     </>
   );
 }
